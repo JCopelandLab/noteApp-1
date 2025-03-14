70638cf0b036ae423df041b83f160f0ba0562b71
JCopelandLab <JTCopeland@proton.me>
2025-03-14 03:33:06 -0400

    vite config changes

--- frontend_noteApp/vite.config.js
+++ frontend_noteApp/vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/notes": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/notes": {
+  //       target: "http://localhost:5173",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 });
